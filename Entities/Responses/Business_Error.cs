110e7e87ae7f16ea506ef4972458b041b417f5f3
Victor Olvera <volvera.thome@outlook.com>
2022-03-15 16:19:33 -0600

    Error entities

---
+++ Entities/Responses/Business_Error.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Responses
+{
+    public class Business_Error
+    {
+        public string Error { get; set; }
+        public string Description { get; set; }
+    }
+}
