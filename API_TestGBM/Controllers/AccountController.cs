6c32e6404919cc790e58ba63c73e9a05849bc636
Victor Olvera <volvera.thome@outlook.com>
2022-03-15 18:10:31 -0600

    Account API Controller complete

--- API_TestGBM/Controllers/AccountController.cs
+++ API_TestGBM/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entities;
+using Business;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +14,16 @@ namespace API_TestGBM.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        // GET: api/<AccountController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<AccountController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        
 
         // POST api/<AccountController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public Account Post([FromBody] Account account)
         {
+            B_Account.CeateAccount(account);
+            return account;
         }
 
-        // PUT api/<AccountController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<AccountController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
